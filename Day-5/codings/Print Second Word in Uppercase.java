5d67803c9351927351903af0c9ecfeb123d6bcf9
Vidyasagar <90808028+vidyasgar@users.noreply.github.com>
2023-02-10 14:13:16 +0530

    Create Print Second Word in Uppercase.java

---
+++ Day-5/codings/Print Second Word in Uppercase.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import  java.util.*;
+class UserMain(){
+public String SecondWOrd(String input1){
+       String [] sp=input1.split(" ");
+        int l=sp.length;
+        if(l==1){
+            return "LESS";
+
+        }
+        else{
+            return sp[1].toUpperCase();
+        }
+
+
+        
+         
+         
+
+         
+	}
+}
